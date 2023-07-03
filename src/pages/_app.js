b8ed4a4b533068941c528c3cf1b50293f36fd6d5
Tejas108 <tejas.monteverdi@gmail.com>
2023-07-03 17:03:59 -0400

    poc remove head/foot

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,11 +6,11 @@ import Footer from '../components/Footer';
 function App({ Component, pageProps }) {
   return (
     <PlayerProvider>
-      <header>
+      {/* <header>
         <Header />
-      </header>
+      </header> */}
       <Component {...pageProps} />
-      <Footer />
+      {/* <Footer /> */}
     </PlayerProvider>
   );
 }
