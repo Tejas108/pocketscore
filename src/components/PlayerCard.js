aa1ef8a515173f80b8988a6c8199912a337ec3aa
Tejas108 <tejas.monteverdi@gmail.com>
2023-07-06 15:08:25 -0400

    tweak

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -17,6 +17,7 @@ function PlayerCard({ player, currentPlayer }) {
   return (
     <div className={`player-card ${isWinner ? 'winner' : ''} ${currentPlayer ? 'current-player' : ''}`}>
       <h3>{player.name}</h3>
+      <div className="divider"><span>wins</span></div>
       <p><span>Games:</span> <span>{player.gamesWon}</span></p>
       <p><span>Matches:</span> <span>{player.matchesWon}</span></p>
     </div>
