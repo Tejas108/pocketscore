6d0c50ed8718b881ebf073e30bde6cf44eb7e20c
Tejas108 <tejas.monteverdi@gmail.com>
2023-07-06 11:27:57 -0400

    install sass. clean up css. change colors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import { PlayerProvider } from '../context/PlayerContext';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
