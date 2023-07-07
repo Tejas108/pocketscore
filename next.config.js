56a60b8ba9c6a9b54ee3681b51426f4ded49f8cc
Tejas108 <tejas.monteverdi@gmail.com>
2023-07-07 16:52:06 -0400

    pwa test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const withPWA = require("next-pwa")({
-    dest: "public",
-    register: true,
-    //disable: process.env.NODE_ENV === "development",
-});
+const withPWA = require("next-pwa");
 
-// Use `withPWA` and pass general Next.js config
-module.exports = withPWA({    
-    reactStrictMode: true,
-    output: "standalone"
+module.exports = withPWA({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	// display: "standalone",
+	//disable: process.env.NODE_ENV === 'development',
 });
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto"></link>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=MuseoModerno"></link>
         <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,400;1,300&display=swap" rel="stylesheet"></link>
