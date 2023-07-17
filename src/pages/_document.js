801a190fc42a684aae6134a5540348b25b5f9565
Tejas108 <tejas.monteverdi@gmail.com>
2023-07-17 11:17:50 -0400

    update link

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"></link>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap"></link>
         <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,400;1,300&display=swap" rel="stylesheet"></link>
-        {/* <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-PN8G48X"/> */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-GRLJME9YFK"/>
       </Head>
       <body>
         <Main />
